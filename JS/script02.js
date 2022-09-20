9b9c0f868e0f35265e0db9dadcc08df2d6bfd704
Mehadi Hasan <themehadihasan@gmail.com>
2022-09-20 23:10:11 +0530

    Initial Commit

--- JS/script02.js
+++ JS/script02.js
@@ -6,16 +6,34 @@ const movieSelect = document.getElementById("movie");
 
 let ticketPrice = parseInt(movieSelect.value);
 
+//save selected movie index and price
+function setmovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .selected");
     const selectedSeatsCount = selectedSeats.length;
+
+    const seatsIndex = [...selectedSeats].map((seat) => {
+        return [...seats].indexOf(seat);
+    });
+
+
+    //set local storage
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+    //seats and price
     count.innerHTML = selectedSeatsCount;
     total.innerHTML = selectedSeatsCount * ticketPrice;
 }
 
+//movie select
 movieSelect.addEventListener("change", (e) => {
     ticketPrice = parseInt(e.target.value);
+    setmovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
